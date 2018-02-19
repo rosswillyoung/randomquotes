1e4a33525b3d87c16338670688f376bbee2d75e6
rosswillyoung <rosswillyoung@gmail.com>
2018-02-19 09:47:39 -0500

    Added a tweet button and a refresh button

--- myscript.js
+++ myscript.js
@@ -2,9 +2,12 @@ let url = "https://talaikis.com/api/quotes/random/";
 let quote = "";
 function getQuote() {
     $.getJSON(url, function(a) {
-        $("#quoteContainer").html(a.quote);
+        $("#quote").html('"' + a.quote + '"');
+        $("#author").html("-" + a.author);
+        $("#tweet").attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + a.quote + '"' + '-' + a.author));
     })
 };
 getQuote();
+
 
 
